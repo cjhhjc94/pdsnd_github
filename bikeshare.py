8b62f54ece4cb2de931e2467c42a68c44b66d8a1
junhyungcha <cjhhjc94@gmail.com>
2020-02-24 01:22:48 +0900

    added underlines to timestats

--- bikeshare.py
+++ bikeshare.py
@@ -18,14 +18,14 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    while True: 
+    while True:
         city = input("Select a city from {}, {} or {}:".format(*CITY_DATA.keys())).strip().lower()
         if city in CITY_DATA.keys():
             break
 
     # TO DO: get user input for month (all, january, february, ... , june)
     while True:
-        
+
         month = input('month to check:').lower()
         if month not in months:
             print('re-enter month')
@@ -79,16 +79,16 @@ def time_stats(df):
 
     # TO DO: display the most common month
     print(df['month'].value_counts().max())
-    mostcommonmonth = df['month'].value_counts().max()
-    print("most common month is ", mostcommonmonth)
+    most_common_month = df['month'].value_counts().max()
+    print("most common month is ", most_common_month)
 
     # TO DO: display the most common day of week
-    mostcommonday = df['day'].value_counts().max()
-    print("most common day is", mostcommonday)
+    most_common_day = df['day'].value_counts().max()
+    print("most common day is", most_common_day)
 
     # TO DO: display the most common start hour
-    mostcommonhour = df['hour'].value_counts().max()
-    print("most common hour is", mostcommonhour)
+    most_common_hour = df['hour'].value_counts().max()
+    print("most common hour is", most_common_hour)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -144,7 +144,7 @@ def user_stats(df):
     print("counts of user types:\n")
     usercounts = df['User Type'].value_counts()
     print(usercounts)
-          
+
     # TO DO: Display counts of gender
     print("counts of gender:\n")
     try:
@@ -154,7 +154,7 @@ def user_stats(df):
         print()
     except KeyError:
         print("There isn't a [Gender] column in this spreedsheet!")
-          
+
     # TO DO: Display earliest, most recent, and most common year of birth
     try:
         birthyear = df['Birth Year']
@@ -170,8 +170,8 @@ def user_stats(df):
         print("There isn't a [Birth Year] column in this spreedsheet!")
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-    
-    
+
+
 def raw_data(df):
     """shows 5 rows of data"""
     show_rows = 5
